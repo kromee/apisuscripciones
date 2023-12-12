1267f60b8b0a606298ee43d375ef8749e44d46c5
Eduardo Dguez <renixedu@gmail.com>
2023-12-12 10:00:51 -0600

    api net6, suscripciones
    api net core 6, suscripciones

---
+++ WebAPIAutores/WebAPIAutores/Migrations/20231208194903_crearbd.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebAPIAutores.Migrations
+{
+    public partial class crearbd : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
